db2638f7bd8d8cf3b27a461450fbdfbca4aecbc5
Nguyen Hoa <hoathaigm@gmail.com>
2022-05-20 13:08:21 +0700

    xay dung UI Header #1 20-05-2022 1:08

--- tiktok/src/components/Layout/components/Header/index.js
+++ tiktok/src/components/Layout/components/Header/index.js
@@ -1,14 +1,33 @@
 import classNames from 'classnames/bind';
-import styles from './Header.module.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import { faCircleXmark,faMagnifyingGlass,faSpinner } from '@fortawesome/free-solid-svg-icons';
 
-const cx= classNames.bind(styles)
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <header className={cx('wrapper')}>
-        <div className={cx('inner')}>
-            {/* logo */}
-            {/* search */}
+  return (
+    <header className={cx('wrapper')}>
+      <div className={cx('inner')}>
+        <div className={cx('logo')}>
+          <img src={images.logo} alt="tiktok"></img>
         </div>
+        <div className={cx('search')}>
+          <input placeholder="Search account and videos" spellCheck={false}></input>
+          <button className={cx('clear')}>
+            <FontAwesomeIcon icon={faCircleXmark}></FontAwesomeIcon>
+          </button>
+          <FontAwesomeIcon className={cx('loading')} icon={faSpinner}></FontAwesomeIcon>
+          {/**Loading */}
+          <button className={cx('search-btn')}>
+          <FontAwesomeIcon icon={faMagnifyingGlass}></FontAwesomeIcon>
+          </button>
+        </div>
+        <div className={cx('actions')}></div>
+      </div>
     </header>
+  );
 }
 
 export default Header;
